4c06e958a675efc667cac58c5d6255551c618c75
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-10-18 10:08:38 +0200

    Add project files.

---
+++ AsyncKeyedLockBenchmarks/Program.cs
@@ -0,0 +1,68 @@
+﻿using AsyncKeyedLock;
+using KeyedSemaphores;
+using ListShuffle;
+using System.Diagnostics;
+
+List<string> myList = new();
+for (int i = 1; i <= 500; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        myList.Add(i.ToString());
+    }
+}
+
+myList.Shuffle();
+var asyncKeyedLocker = new AsyncKeyedLocker();
+
+{
+    // Discard these results
+    var result = Parallel.For(0, 10, async (i, state) =>
+    {
+        using (await asyncKeyedLocker.LockAsync(myList[i]))
+        {
+            await Task.Delay(1);
+        }
+    });
+    Console.WriteLine($"Initialised AsyncKeyedLock. Starting benchmark...");
+}
+
+{
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    var result = Parallel.For(0, myList.Count, async (i, state) =>
+    {
+        using (await asyncKeyedLocker.LockAsync(myList[i]))
+        {
+            await Task.Delay(1);
+        }
+    });
+    stopwatch.Stop();
+    Console.WriteLine($"AsyncKeyedLock took {stopwatch.ElapsedMilliseconds}ms");
+}
+
+{
+    // Discard these results
+    var result = Parallel.For(0, 10, async (i, state) =>
+    {
+        using (await KeyedSemaphore.LockAsync(myList[i]))
+        {
+            await Task.Delay(1);
+        }
+    });
+    Console.WriteLine($"Initialised KeyedSemaphores. Starting benchmark...");
+}
+
+{
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    var result = Parallel.For(0, myList.Count, async (i, state) =>
+    {
+        using (await KeyedSemaphore.LockAsync(myList[i]))
+        {
+            await Task.Delay(1);
+        }
+    });
+    stopwatch.Stop();
+    Console.WriteLine($"KeyedSemaphores took {stopwatch.ElapsedMilliseconds}ms");
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+This is a console application benchmarking two similar libraries, [AsyncKeyedLock](https://github.com/MarkCiliaVincenti/AsyncKeyedLock) and [KeyedSemaphores](https://github.com/amoerie/keyed-semaphores).
+
+The purpose of these libraries are to lock based on a key. This has many applications, for example a bank might want to process transactions concurrently but only one at a time for the same bank account.
+
+In these tests, we first build up data, shuffle it using [ListShuffle](https://github.com/MarkCiliaVincenti/ListShuffle), initialize both libraries and then run the tests.
+
+The results consistently show [AsyncKeyedLock](https://github.com/MarkCiliaVincenti/AsyncKeyedLock) to be considerably more performant.
+
+Typical results:
+
+````
+Initialised AsyncKeyedLock. Starting benchmark...
+AsyncKeyedLock took 15355ms
+Initialised KeyedSemaphores. Starting benchmark...
+KeyedSemaphores took 40444ms
+````
