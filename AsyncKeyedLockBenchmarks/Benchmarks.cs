6cb4c6440e9608626efbf01ac6f9d5c9051b0ed6
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-12-19 08:48:48 +0100

    u

--- AsyncKeyedLockBenchmarks/Benchmarks.cs
+++ AsyncKeyedLockBenchmarks/Benchmarks.cs
@@ -12,6 +12,8 @@ namespace AsyncKeyedLockBenchmarks
 {
     [Config(typeof(Config))]
     [MemoryDiagnoser]
+    [JsonExporterAttribute.Full]
+    [JsonExporterAttribute.FullCompressed]
     public class Benchmarks
     {
         private class Config : ManualConfig
