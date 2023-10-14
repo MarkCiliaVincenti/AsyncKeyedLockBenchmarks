94776a6fb72a29fb9f2cd2e19a8c8d78c3c6e0e8
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2023-10-14 11:32:40 +0200

    added nopooling

--- AsyncKeyedLockBenchmarks/Benchmarks.cs
+++ AsyncKeyedLockBenchmarks/Benchmarks.cs
@@ -159,6 +159,7 @@ namespace AsyncKeyedLockBenchmarks
             AsyncKeyedLockerNoPoolingTasks = null;
         }
 
+        [Benchmark]
         public async Task AsyncKeyedLockNoPooling()
         {
 #pragma warning disable CS8604 // Possible null reference argument.
