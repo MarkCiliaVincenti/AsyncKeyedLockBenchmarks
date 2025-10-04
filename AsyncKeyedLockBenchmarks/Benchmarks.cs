ab2b124db01535aac7214c8c662a6fa14a78e60e
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2025-10-04 16:34:03 +0200

    7.1.6 to 7.1.7

--- AsyncKeyedLockBenchmarks/Benchmarks.cs
+++ AsyncKeyedLockBenchmarks/Benchmarks.cs
@@ -30,8 +30,8 @@ namespace AsyncKeyedLockBenchmarks
             {
                 var baseJob = Job.Default;
 
-                AddJob(baseJob.WithNuGet("AsyncKeyedLock", "7.1.4").WithBaseline(true));
-                AddJob(baseJob.WithNuGet("AsyncKeyedLock", "7.1.5-alpha"));
+                AddJob(baseJob.WithNuGet("AsyncKeyedLock", "7.1.6").WithBaseline(true));
+                AddJob(baseJob.WithNuGet("AsyncKeyedLock", "7.1.7"));
             }
         }
 
